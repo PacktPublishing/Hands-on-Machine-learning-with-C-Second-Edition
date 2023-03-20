7dbb54f188f0c5d50f5d06104bb89b2f0ced5622
kolkir <rotate@ukr.net>
2023-03-20 19:43:57 +0200

    Include compilation of all libraries into the dockerfile

--- env_scripts/install_env.sh
+++ env_scripts/install_env.sh
@@ -8,34 +8,34 @@ mkdir -p libs
 mkdir -p libs/sources
 
 # Blaze
-#. ./install_lib.sh https://bitbucket.org/blaze-lib/blaze.git v3.8.2
+. ./install_lib.sh https://bitbucket.org/blaze-lib/blaze.git v3.8.2
 
 # Firearray
-#. ./install_lib.sh https://github.com/arrayfire/arrayfire v3.8.3 -DBUILD_TESTS=OFF
+. ./install_lib.sh https://github.com/arrayfire/arrayfire v3.8.3 -DBUILD_TESTS=OFF
 
 # DLib
-#. ./install_lib.sh https://github.com/davisking/dlib v19.24
+. ./install_lib.sh https://github.com/davisking/dlib v19.24
 
 # Armadillo
-#. ./install_lib.sh https://gitlab.com/conradsnicta/armadillo-code 12.0.x
+. ./install_lib.sh https://gitlab.com/conradsnicta/armadillo-code 12.0.x
 
 # xtl
-#. ./install_lib.sh https://github.com/xtensor-stack/xtl 0.7.5
+. ./install_lib.sh https://github.com/xtensor-stack/xtl 0.7.5
 
 # xtensor
-#. ./install_lib.sh https://github.com/xtensor-stack/xtensor 0.24.5
+. ./install_lib.sh https://github.com/xtensor-stack/xtensor 0.24.5
 
 # xtensor-blas
-#. ./install_lib.sh https://github.com/xtensor-stack/xtensor-blas 0.20.0
+. ./install_lib.sh https://github.com/xtensor-stack/xtensor-blas 0.20.0
 
 # NlohmanJson
-#. ./install_lib.sh https://github.com/nlohmann/json.git v3.11.2 -DJSON_BuildTests=OFF
+. ./install_lib.sh https://github.com/nlohmann/json.git v3.11.2 -DJSON_BuildTests=OFF
 
 # mlpack
-#. ./install_lib.sh https://github.com/mlpack/mlpack 4.0.1 -DBUILD_PYTHON_BINDINGS=OFF -DBUILD_TESTS=OFF -DDOWNLOAD_DEPENDENCIES=ON
+. ./install_lib.sh https://github.com/mlpack/mlpack 4.0.1 -DBUILD_PYTHON_BINDINGS=OFF -DBUILD_TESTS=OFF -DDOWNLOAD_DEPENDENCIES=ON
 
 # Eigen
-#. ./install_lib.sh https://gitlab.com/libeigen/eigen.git 3.4.0
+. ./install_lib.sh https://gitlab.com/libeigen/eigen.git 3.4.0
 
 # PyTorch
 . ./install_lib.sh https://github.com/pytorch/pytorch v1.13.1 -DBUILD_PYTHON=OFF -DONNX_NAMESPACE=onnx_torch

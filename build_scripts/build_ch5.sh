08c4ff1e4b7533a902f347401b83093a96b976c8
kolkir <kirill.tomorrow@gmail.com>
2023-09-10 16:40:06 +0300

    Fix build scripts for chapters 4 and 5

--- build_scripts/build_ch5.sh
+++ build_scripts/build_ch5.sh
@@ -3,14 +3,14 @@ START_DIR=${PWD%/*}
 # change this directory path according to your configuration
 LIBS_DIR=/development/libs
 
-#Chapter 3
+#Chapter 5
 cd $START_DIR/Chapter05/dlib
 mkdir build
 cd build/
 cmake cmake -DCMAKE_PREFIX_PATH=/development/libs/ -DPLOTCPP_PATH=$LIBS_DIR/sources/plotcpp/ ..
 cmake --build . --target all
 
-cd $START_DIR/Chapter03/mlpack
+cd $START_DIR/Chapter05/mlpack
 mkdir build
 cd build/
 cmake cmake -DCMAKE_PREFIX_PATH=/development/libs/ -DPLOTCPP_PATH=$LIBS_DIR/sources/plotcpp/ ..
